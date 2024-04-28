6e620558a2bfb89310fe9c1086417e0409671e88
Himanshu4256 <mhimanshu2104@gmail.com>
2024-04-28 17:44:50 +0530

    project first commmit

---
+++ src/main/java/com/code/JavaGate/Secure/Access/Hub/JavaGateSecureAccessHubApplication.java
@@ -0,0 +1,13 @@
+package com.code.JavaGate.Secure.Access.Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGateSecureAccessHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGateSecureAccessHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/JavaGate/Secure/Access/Hub/JavaGateSecureAccessHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.JavaGate.Secure.Access.Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGateSecureAccessHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
